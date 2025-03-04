9f0cadee25a0015ab325ac21dfb525c1fc23c450
pwups <aylapmacc@gmail.com>
2025-03-04 21:42:41 +0800

    Create messageFormatter.js

---
+++ utils/messageFormatter.js
@@ -0,0 +1,11 @@
+function formatCloseMessage(reason) {
+    return `_ _
+-# _ _⠀⠀\` ╬═ \`   ˚ ₊   **your ticket has been closed**
+⠀⠀ʚɞ⠀*reason*⠀﹕⠀${reason}
+⠀⠀⠀⠀  ᶠᵉᵉˡ ᶠʳᵉᵉ ᵗᵒ ᶜᵒᵐᵉ ᵇᵃᶜᵏ ˢᵒᵒⁿ    *!*
+_ _`;
+}
+
+module.exports = {
+    formatCloseMessage
+};
